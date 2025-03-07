b9bf8197cd1abd805dfcbba207f00fe2a08ab1b3
phungxuangiap <xuangiap0806@gmail.com>
2025-03-07 23:39:00 +0700

    calendar version 2

--- src/screens/CalendarScreen.tsx
+++ src/screens/CalendarScreen.tsx
@@ -2,27 +2,20 @@ import { Dimensions, ScrollView, Text, Touchable, TouchableOpacity, View } from 
 import { Calendar } from 'react-native-big-calendar';
 import { AppPadding } from '../constants/spaces';
 import { useState } from 'react';
-import { GrayColor, GreenColor, NotificationColor, PrimaryColorBlue, PrimaryColorRed, PurpleColor, WhiteColor } from '../assets/color';
+import { BackgroundColor, GrayColor, GreenColor, NotificationColor, PrimaryColorBlue, PrimaryColorRed, PurpleColor, WhiteColor } from '../assets/color';
 import { Title } from '../components/Title';
 import { Space } from '../components/Space';
 import UUID from 'react-native-uuid';
+import { counterEvent } from 'react-native/Libraries/Performance/Systrace';
+import { OverlayBubbleAnimation } from '../components/OverlayBubbleAnimation';
 
 const {height} = Dimensions.get('window')
-const {width} = Dimensions.get('window')
-
 export const CalendarScreen = () => {
     const [events, setEvents] = useState([
+
     {
         id: UUID.v4(),
         title: 'Meeting1',
-        start: new Date(2025, 2, 7, 10, 1),
-        end: new Date(2025, 2, 7, 12, 30),
-        color: PurpleColor
-    },
-    {
-        id: UUID.v4(),
-
-        title: 'Meeting2',
         start: new Date(2025, 2, 7, 11, 1),
         end: new Date(2025, 2, 7, 13, 30),
         color: GrayColor
@@ -31,8 +24,8 @@ export const CalendarScreen = () => {
         id: UUID.v4(),
 
         title: 'Meeting5',
-        start: new Date(2025, 2, 7, 11, 3),
-        end: new Date(2025, 2, 7, 13, 3),
+        start: new Date(2025, 2, 7, 11, 2),
+        end: new Date(2025, 2, 7, 15, 3),
         color: GreenColor
     },
     {
@@ -40,108 +33,82 @@ export const CalendarScreen = () => {
 
         title: 'Meeting5',
         start: new Date(2025, 2, 7, 11, 3),
-        end: new Date(2025, 2, 7, 13, 3),
+        end: new Date(2025, 2, 7, 15, 3),
         color: GreenColor
     },
     {
         id: UUID.v4(),
 
         title: 'Meeting5',
-        start: new Date(2025, 2, 7, 11, 3),
-        end: new Date(2025, 2, 7, 13, 3),
+        start: new Date(2025, 2, 7, 11, 4),
+        end: new Date(2025, 2, 7, 15, 3),
         color: GreenColor
     },
     {
         id: UUID.v4(),
 
-        title: 'Meeting6',
-        start: new Date(2025, 2, 7, 15, 3),
-        end: new Date(2025, 2, 7, 16, 3),
+        title: 'Meeting8: Planning new Things for this company next year',
+        start: new Date(2025, 2, 7, 16, 3),
+        end: new Date(2025, 2, 7, 20, 3),
         color: GreenColor
     },
-    {
-        id: UUID.v4(),
-
-        title: 'Meeting7',
-        start: new Date(2025, 2, 7, 15, 3),
-        end: new Date(2025, 2, 7, 16, 3),
-        color: GreenColor
-    },
-     {
-        id: UUID.v4(),
-
-        title: 'Meeting7',
-        start: new Date(2025, 2, 7, 15, 3),
-        end: new Date(2025, 2, 7, 16, 3),
-        color: GreenColor
-    },
-     {
-        id: UUID.v4(),
-
-        title: 'Meeting7',
-        start: new Date(2025, 2, 7, 15, 3),
-        end: new Date(2025, 2, 7, 16, 3),
-        color: GreenColor
-    },
-     {
-        id: UUID.v4(),
-
-        title: 'Meeting7',
-        start: new Date(2025, 2, 7, 15, 3),
-        end: new Date(2025, 2, 7, 16, 3),
-        color: GreenColor
-    },
-     {
-        id: UUID.v4(),
-
-        title: 'Meeting7',
-        start: new Date(2025, 2, 7, 15, 3),
-        end: new Date(2025, 2, 7, 16, 3),
-        color: GreenColor
-    },
-     {
-        id: UUID.v4(),
-
-        title: 'Meeting7',
-        start: new Date(2025, 2, 7, 15, 3),
-        end: new Date(2025, 2, 7, 16, 3),
-        color: GreenColor
-    },
-    
     ])
-    const [calendarMode, setCalendarMode] = useState('day');
-    return (
-        <View style = {{padding: AppPadding, marginTop: 32}}>
-            <Calendar eventCellStyle={{justifyContent: 'center', backgroundColor: 'transparent', borderRadius: 20, minWidth: 0, margin: 0, position: 'absolute', shadowColor: 'transparent'}}
-            hourStyle={{fontFamily: 'DynaPuff-Bold', color: GrayColor}}
-            
-            renderEvent={(event, touchableOpacityProps)=>{
-                let count = 0;
-                let countMargin = 0;
-                let foundEvent = false;
-                for (let i = 0; i<events.length; i++){
-                    if ((event.start<=events[i].end && event.start >= events[i].start) || (event.end<=events[i].end && event.end>=events[i].start) || (event.start<=events[i].start && event.end>=events[i].end)){
-                        count++;
-                        if (event.id === events[i].id){foundEvent = true};
-                        if (!foundEvent){
-                            countMargin++;
-                        }
-                    }
+    const [calendarMode, setCalendarMode] = useState('day')
+    const [width, setWidth] = useState(0);
+    let maxCount: number = 0;
+    for (let event of events){
+        let count = 0;
+        let countMargin = 0;
+        let foundEvent = false;
+        for (let i = 0; i<events.length; i++){
+            if ((event.start<=events[i].end && event.start >= events[i].start) || (event.end<=events[i].end && event.end>=events[i].start) || (event.start<=events[i].start && event.end>=events[i].end)){
+                count++;
+                if (event.id === events[i].id){foundEvent = true};
+                if (!foundEvent){
+                    countMargin++;
                 }
-                const len = (width*80/100)/(count);
-                let eventProps = touchableOpacityProps
-                return (
-                    <TouchableOpacity {...eventProps} onPress={()=>{
-                    }} key={UUID.v4()}>
-                        <View style ={{marginLeft: len*countMargin, flexDirection: 'row', flex: 1, alignItems: 'center', width: len, backgroundColor: event.color, borderRadius: 14, shadowColor: '#000', shadowRadius: 20, shadowOffset: {width: 0, height: 4}, elevation: 5}}>
+            }
+        }
+        if (count>maxCount){
+            maxCount = count;
+        }
+    }
+    return (
+        <View style = {{padding: AppPadding, paddingTop: 32, position: 'relative'}} onLayout={(event)=>{
+                let setValue = event.nativeEvent.layout.width;
+                if (calendarMode === '3days'){
+                    setValue = event.nativeEvent.layout.width / 3
+                } else if (calendarMode === 'week'){
+                    setValue = event.nativeEvent.layout.width / 7
+                }
+                setWidth(setValue)
+            }}
+        >
+            <OverlayBubbleAnimation/>
 
-                            <Title title={event.title} color={WhiteColor} size={8} type={true} horizontalPadding={12} verticalPadding={0}/>
-                        </View>
-                    </TouchableOpacity>
-                )
+            <Calendar eventCellStyle={{justifyContent: 'center', backgroundColor: 'transparent', width: ((width*0.8)/(maxCount)),  borderRadius: 12, minWidth: 0, margin: 0, position: 'absolute', shadowColor: 'transparent'}}
+            hourStyle={{fontFamily: 'DynaPuff-Bold', color: GrayColor}}
+            overlapOffset={((width*0.73)/(maxCount))}
+            renderEvent={(event, touchableOpacityProps)=>{
+                let eventProps = touchableOpacityProps
+                if (calendarMode!=='month'){
+
+                    return (
+                        <TouchableOpacity {...eventProps} onPress={()=>{
+                        }} key={UUID.v4()}>
+                            <View style ={{flexDirection: 'row', justifyContent: 'center', flex: 1, alignItems: 'center', backgroundColor: event.color, borderRadius: 10, position: 'relative', borderWidth: 1, borderColor: WhiteColor}}>
+                                <View style = {{height: 10, width: 10, backgroundColor: '#fff', opacity: 0.1, borderRadius: 20, position: 'absolute', top: 5, left: 5}}/>
+                                <Title title={event.title} color={WhiteColor} size={8} type={true} horizontalPadding={6} verticalPadding={0}/>
+                            </View>
+                        </TouchableOpacity>
+                    )
+                } else{
+                    return (
+                        <View style={{width: 10, height: 10, borderRadius: 10, backgroundColor: event.color}}></View>
+                    )
+                }
             }}
             events={events} height={height} mode={calendarMode || 'day'} onPressDateHeader={()=>{}} showTime={true} hideNowIndicator={false} onPressEvent={(event)=>{console.log(event)}}/>
-            <Space space={50}/>
         </View>
     );
 
